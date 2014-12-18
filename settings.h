172c207c595f2a36a71b68fb34b8cb9a454a0dca
Andrew Pullin <pullin@berkeley.edu>
2014-12-18 13:09:48 -0800

    Addint IDENT_STR to settings.h

--- settings.h
+++ settings.h
@@ -7,10 +7,12 @@
 #ifndef __SETTINGS_H
 #define __SETTINGS_H
 
+#define IDENT_STR "BIOMIMETICS-ROACH;AMS-ENC;"
+
 /////// Radio settings ///////
 #define RADIO_CHANNEL		0x19
 #define RADIO_SRC_ADDR 		0x2052
-#define RADIO_PAN_ID            0x2050
+#define RADIO_PAN_ID        0x2050
 //Hard-coded destination address, must match basestation or XBee addr
 #define RADIO_DST_ADDR		0x2051
 
