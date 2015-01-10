4a6758d637c67d03c38eb420849f9fa382ca41bb
Andrew Pullin <pullin@berkeley.edu>
2015-01-09 16:44:47 -0800

    Removing settings which are not yet implmented yet.

--- settings.h
+++ settings.h
@@ -25,17 +25,7 @@
 #define TELEMPACKFUNC(x) vrTelemGetData(x)
 
 // Encoders Setup
-//Left legs
-#define LEFT_LEGS_PID_NUM       0       //PID module index is 0-3
-#define LEFT_LEGS_ENC_NUM       1       //amsEnc module index is 0-3
-#define AMS_ENC_OFFSET_1        7706
-#define LEFT_LEG_FLIP           1       //"forward" reversed for left
-#define LEFT_LEGS_TIH_CHAN      1       //tiH module index is 1-4
-//Right legs
-#define RIGHT_LEGS_PID_NUM      1       //PID module index is 0-3
-#define RIGHT_LEGS_ENC_NUM      0       //amsEnc module index is 0-3
 #define AMS_ENC_OFFSET_0        5758
-#define RIGHT_LEG_FLIP          0       //"forward" regular for right
-#define RIGHT_LEGS_TIH_CHAN     2       //tiH module index is 1-4
+#define AMS_ENC_OFFSET_1        7706
 
 #endif //_SETTINGS_H
